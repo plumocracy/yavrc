4f613612e9524c97a7f625686e5df4c288a65be4
Plum <evelynplumhill@gmail.com>
2026-09-16 11:19:13 -0400

    Re-Added push prompt

--- lua/after/git.lua
+++ lua/after/git.lua
@@ -17,10 +17,30 @@ vim.keymap.set("n", "<leader>gac", function()
 				vim.system({ "git", "commit", "-m", msg }, {}, function(commitResult)
 					if commitResult.code ~= 0 then
 						vim.schedule(function()
-							vim.notify(commitResult.stderr, vim.log.levels.ERROR)
+							Snacks.notify.error(commitResult.stderr)
+							return
 						end)
 					end
 				end)
+				
+				Snacks.input({
+					prompt = "Push changes now? y/n"
+				}, function(msg) 
+					if msg == 'y' or msg == 'yes' then
+						vim.system({ "git", "push" }, {}, function(pushResult) 
+							if pushResult.code ~= 0 then
+								Snacks.notify.error(pushResult.stderr)
+							end
+						end)
+					end
+					
+					if msg == 'n' or msg == 'no' then
+						return
+					end
+
+					Snacks.notify.error("Invalid input, no action taken.")
+				end)
+
 			end)
 		end)
 	end)
