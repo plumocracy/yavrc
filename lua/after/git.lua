56a1f69ce7ef2dd9cc127b06aeaa3f535d663d46
Plum <evelynplumhill@gmail.com>
2026-09-16 11:12:55 -0400

    Added better notifications

--- lua/after/git.lua
+++ lua/after/git.lua
@@ -10,6 +10,25 @@ vim.keymap.set("n", "<leader>gac", function()
 		end		
 
 		vim.cmd([[Git add -A]])
-		vim.cmd("Git commit -m " .. vim.fn.shellescape(value))
+		vim.cmd("silent Git commit -m " .. vim.fn.shellescape(value))	
+
+
+		Snacks.input({
+			prompt = "Push? y/n"
+		}, function(value) 
+			if value == 'n' or value == 'no' then
+				return
+			end
+
+			if value == 'y' or value == 'yes' then
+				vim.cmd([[silent Git push]])
+				Snacks.notiify.info("Pushed to remote.")
+			end
+
+			Snacks.notiify.erro("Invalid response. No action taken.")
+
+			return
+		end)
 	end)
+
 end)
